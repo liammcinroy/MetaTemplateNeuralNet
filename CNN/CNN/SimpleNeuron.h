fe7a830868728d981a3c4df958368035b92c9a8c
William McInroy <lemur1070@hotmail.com>
2014-06-04 18:09:53 -0600

    Updated ignore, and started constructor

--- CNN/CNN/SimpleNeuron.h
+++ CNN/CNN/SimpleNeuron.h
@@ -9,6 +9,22 @@ public:
 	int GetLayer();
 	int GetIndex();
 	void SetValue(float);
+	friend bool operator<(SimpleNeuron first, SimpleNeuron other)
+	{
+		if (other.GetLayer() < first.GetLayer())
+			return true;
+
+		else if (other.GetLayer() == first.GetLayer())
+		{
+			if (other.GetIndex() < first.GetIndex())
+				return true;
+			else
+				return false;
+		}
+
+		else
+			return false;
+	}
 private:
 	float m_Output;
 	int m_Layer;
